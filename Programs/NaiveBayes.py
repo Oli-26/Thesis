d24cad1f1bb26bd90c60ca020b256b2c90f5505e
Oli-26 <Olivermholder@gmail.com>
2020-05-16 12:34:16 +0200

    changed algorithm to run in Pipe, moved most code inside functions. Added some comments.

--- Programs/NaiveBayes.py
+++ Programs/NaiveBayes.py
@@ -7,44 +7,65 @@ address = 'technical_debt_dataset.csv'
 
 
 
+
+def print_categories(df):
+    # Conversion form classification to category id(Useful for deciphering output)
+    category_id_df = df[['classification', 'category_id']].drop_duplicates().sort_values('category_id')
+    category_to_id = dict(category_id_df.values)
+    id_to_category = dict(category_id_df[['category_id', 'classification']].values)
+    df.head()
+    print(id_to_category)
+
+def plot_classification_frequency(df):
+    # Plot count of different types
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(8,6))
+    df.groupby('classification').commenttext.count().plot.bar(ylim=0)
+    plt.show()
+
+
+    
+def extract_features(df):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+    features = tfidf.fit_transform(df.commenttext).toarray()
+    labels = df.category_id
+    print(features.shape)
+
+def train_model(df):
+    #Create learning model
+    from sklearn.model_selection import train_test_split
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfTransformer
+    from sklearn.naive_bayes import MultinomialNB
+    X_train, X_test, y_train, y_test = train_test_split(df['commenttext'], df['category_id'], random_state = 14)
+
+    # Pipe countvectorizer, tfidftransformer, and multinomial naivebayes together.
+    from sklearn.pipeline import Pipeline
+    text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
+    text_clf.fit(X_train, y_train)
+    
+    
+    # return pipe.
+    return text_clf
+
+
+def examples(clf):
+    # Test examples
+    print(clf.predict(["This is poorly designed. We should change the char x to an int and then cast it later."]))
+
+
+ 
+    
+    
+    
+    
+#MainBody
 from LoadData import load_from_file
 
 # Load dataframe
 df = load_from_file(address, amount = 20000)
 
-
-
-# Conversion form classification to category id(Useful for deciphering output)
-category_id_df = df[['classification', 'category_id']].drop_duplicates().sort_values('category_id')
-category_to_id = dict(category_id_df.values)
-id_to_category = dict(category_id_df[['category_id', 'classification']].values)
-df.head()
-print(id_to_category)
-
-
-# Plot count of different types
-import matplotlib.pyplot as plt
-fig = plt.figure(figsize=(8,6))
-df.groupby('classification').commenttext.count().plot.bar(ylim=0)
-plt.show()
-
-
-#Create learning model
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
-X_train, X_test, y_train, y_test = train_test_split(df['commenttext'], df['category_id'], random_state = 14)
-count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(X_train)
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-clf = MultinomialNB().fit(X_train_tfidf, y_train)
-
-
-
-# Test examples
-print(clf.predict(count_vect.transform(["This is poorly designed. We should change the char x to an int and then cast it later."])))
-
-
-print(clf.predict(count_vect.transform(["// This is design debt"])))
+extract_features(df)
+clf = train_model(df)
+examples(clf)
